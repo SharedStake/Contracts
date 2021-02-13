27b423259d3969afff2551d9532333ff86d82ba2
sharedStake-dev <75868527+sharedStake-dev@users.noreply.github.com>
2021-02-12 18:10:45 -0600

    Create SmartVesting.sol

---
+++ SmartVesting.sol
@@ -0,0 +1,291 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.6.9;
+// pragma solidity 0.8.1;
+
+/**
+ * SharedStake vesting contract
+ * Fork of badger SmartVesting
+ * */
+import {Executor} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/contracts/badger-timelock/Executor.sol";
+
+import {IERC20} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {SafeERC20} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {Ownable} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/access/Ownable.sol";
+import {SafeMath} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/math/SafeMath.sol";
+import {Initializable} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+
+/**
+ * @title SingleTokenVesting
+ * @dev A token holder contract that can release its token balance gradually like a
+ * typical vesting scheme, with a cliff and vesting period. Optionally revocable by the
+ * owner. Only tracks vesting for a single token, rather than all ERC20s.
+ */
+abstract contract SingleTokenVestingNonRevocable is Ownable, Initializable {
+    // The vesting schedule is time-based (i.e. using block timestamps as opposed to e.g. block numbers), and is
+    // therefore sensitive to timestamp manipulation (which is something miners can do, to a certain degree). Therefore,
+    // it is recommended to avoid using short time durations (less than a minute). Typical vesting schemes, with a
+    // cliff period of a year and a duration of four years, are safe to use.
+    // solhint-disable not-rely-on-time
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    event TokensReleased(address token, uint256 amount);
+
+    IERC20 internal _token;
+    // beneficiary of tokens after they are released
+    address private _beneficiary;
+
+    // Durations and timestamps are expressed in UNIX time, the same units as block.timestamp.
+    uint256 private _cliff;
+    uint256 private _start;
+    uint256 private _duration;
+
+    uint256 private _released;
+
+    /**
+     * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
+     * beneficiary, gradually in a linear fashion until start + duration. By then all
+     * of the balance will have vested.
+     * @param token address of the token to vest
+     * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+     * @param cliffDuration duration in seconds of the cliff in which tokens will begin to vest
+     * @param start the time (as Unix time) at which point vesting starts
+     * @param duration duration in seconds of the period in which the tokens will vest
+     */
+    function __SingleTokenVestingNonRevocable_init(
+        IERC20 token,
+        address beneficiary,
+        uint256 start,
+        uint256 cliffDuration,
+        uint256 duration
+        ) public initializer {
+        require(address(token) != address(0), "TokenVesting: token is the zero address");
+        require(beneficiary != address(0), "TokenVesting: beneficiary is the zero address");
+        // solhint-disable-next-line max-line-length
+        require(cliffDuration <= duration, "TokenVesting: cliff is longer than duration");
+        require(duration > 0, "TokenVesting: duration is 0");
+        // solhint-disable-next-line max-line-length
+        require(start.add(duration) > block.timestamp, "TokenVesting: final time is before current time");
+
+        _token = token;
+        _beneficiary = beneficiary;
+        _duration = duration;
+        _cliff = start.add(cliffDuration);
+        _start = start;
+    }
+    
+    constructor() public {}
+
+    /**
+     * @return the token being held.
+     */
+    function token() public view returns (IERC20) {
+        return _token;
+    }
+
+    /**
+     * @return the beneficiary of the tokens.
+     */
+    function beneficiary() public view returns (address) {
+        return _beneficiary;
+    }
+
+    /**
+     * @return the cliff time of the token vesting.
+     */
+    function cliff() public view returns (uint256) {
+        return _cliff;
+    }
+
+    /**
+     * @return the start time of the token vesting.
+     */
+    function start() public view returns (uint256) {
+        return _start;
+    }
+
+    /**
+     * @return the duration of the token vesting.
+     */
+    function duration() public view returns (uint256) {
+        return _duration;
+    }
+
+    /**
+     * @return the amount of the token released.
+     */
+    function released() public view returns (uint256) {
+        return _released;
+    }
+
+    /**
+     * @notice Transfers vested tokens to beneficiary.
+     */
+    function release() public {
+        uint256 unreleased = _releasableAmount();
+
+        require(unreleased > 0, "TokenVesting: no tokens are due");
+
+        _released = _released.add(unreleased);
+
+        _token.safeTransfer(_beneficiary, unreleased);
+
+        emit TokensReleased(address(_token), unreleased);
+    }
+
+    function releasableAmount() public view returns (uint256) {
+        return _releasableAmount();
+    }
+
+    /**
+     * @dev Calculates the amount that has already vested but hasn't been released yet.
+     */
+    function _releasableAmount() private view returns (uint256) {
+        return _vestedAmount().sub(_released);
+    }
+
+    /**
+     * @dev Calculates the amount that has already vested.
+     */
+    function _vestedAmount() private view returns (uint256) {
+        uint256 currentBalance = _token.balanceOf(address(this));
+        uint256 totalBalance = currentBalance.add(_released);
+
+        if (block.timestamp < _cliff) {
+            return 0;
+        } else if (block.timestamp >= _start.add(_duration)) {
+            return totalBalance;
+        } else {
+            return totalBalance.mul(block.timestamp.sub(_start)).div(_duration);
+        }
+    }
+}
+
+/* 
+  A token vesting contract that is capable of interacting with other smart contracts.
+  This allows the beneficiary to participate in on-chain goverance processes, despite having locked tokens.
+  The beneficiary can withdraw the appropriate vested amount at any time.
+
+  Features safety functions to allow beneficiary to claim ETH & ERC20-compliant tokens sent to the timelock contract, accidentially or otherwise.
+
+  An optional 'governor' address has the ability to allow the vesting to send it's tokens to approved destinations. 
+  This is intended to allow the token holder to stake their tokens in approved mechanisms.
+*/
+
+contract SmartVesting is SingleTokenVestingNonRevocable, Executor, ReentrancyGuard {
+    address internal _governor;
+    mapping(address => bool) internal _transferAllowed;
+
+    constructor(
+        IERC20 token,
+        address beneficiary,
+        address governor,
+        uint256 start,
+        uint256 cliffDuration,
+        uint256 duration
+    ) public {
+        __SingleTokenVestingNonRevocable_init(token, beneficiary, start, cliffDuration, duration);
+        _governor = governor;
+    }
+
+    event Call(address to, uint256 value, bytes data, bool transfersAllowed);
+    event ApproveTransfer(address to);
+    event RevokeTransfer(address to);
+    event ClaimToken(IERC20 token, uint256 amount);
+    event ClaimEther(uint256 amount);
+
+    modifier onlyBeneficiary() {
+        require(msg.sender == beneficiary(), "smart-timelock/only-beneficiary");
+        _;
+    }
+
+    modifier onlyGovernor() {
+        require(msg.sender == _governor, "smart-timelock/only-governor");
+        _;
+    }
+
+    /**
+     * @notice Allows the timelock to call arbitrary contracts, as long as it does not reduce it's locked token balance
+     * @dev Initialization check is implicitly provided by `voteExists()` as new votes can only be
+     *      created via `newVote(),` which requires initialization
+     * @param to Contract address to call
+     * @param value ETH value to send, if any
+     * @param data Encoded data to send
+     */
+    function call(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable onlyBeneficiary() nonReentrant() returns (bool success) {
+        uint256 preAmount = token().balanceOf(address(this));
+
+        success = execute(to, value, data, gasleft());
+
+        if (!_transferAllowed[to]) {
+            uint256 postAmount = token().balanceOf(address(this));
+            require(postAmount >= preAmount, "smart-vesting/locked-balance-check");
+        }
+
+        emit Call(to, value, data, _transferAllowed[to]);
+    }
+
+    function approveTransfer(address to) external onlyGovernor() {
+        _transferAllowed[to] = true;
+        emit ApproveTransfer(to);
+    }
+
+    function revokeTransfer(address to) external onlyGovernor() {
+        _transferAllowed[to] = false;
+        emit RevokeTransfer(to);
+    }
+
+    /**
+     * @notice Claim ERC20-compliant tokens other than locked token.
+     * @param tokenToClaim Token to claim balance of.
+     */
+    function claimToken(IERC20 tokenToClaim) external onlyBeneficiary() nonReentrant() {
+        require(address(tokenToClaim) != address(token()), "smart-timelock/no-locked-token-claim");
+        uint256 preAmount = token().balanceOf(address(this));
+
+        uint256 claimableTokenAmount = tokenToClaim.balanceOf(address(this));
+        require(claimableTokenAmount > 0, "smart-timelock/no-token-balance-to-claim");
+
+        tokenToClaim.transfer(beneficiary(), claimableTokenAmount);
+
+        uint256 postAmount = token().balanceOf(address(this));
+        require(postAmount >= preAmount, "smart-timelock/locked-balance-check");
+
+        emit ClaimToken(tokenToClaim, claimableTokenAmount);
+    }
+
+    /**
+     * @notice Claim Ether in contract.
+     */
+    function claimEther() external onlyBeneficiary() nonReentrant() {
+        uint256 preAmount = token().balanceOf(address(this));
+
+        uint256 etherToTransfer = address(this).balance;
+        require(etherToTransfer > 0, "smart-timelock/no-ether-balance-to-claim");
+
+        payable(beneficiary()).transfer(etherToTransfer);
+
+        uint256 postAmount = token().balanceOf(address(this));
+        require(postAmount >= preAmount, "smart-timelock/locked-balance-check");
+
+        emit ClaimEther(etherToTransfer);
+    }
+
+    /**
+     * @notice Governor address
+     */
+    function governor() external view returns (address) {
+        return _governor;
+    }
+
+    /**
+     * @notice Allow timelock to receive Ether
+     */
+    receive() external payable {}
+}
