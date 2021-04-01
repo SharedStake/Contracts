1e6540b41bdd7b61c0a354d603c2eae70b69dd39
sharedStake-dev <75868527+sharedStake-dev@users.noreply.github.com>
2021-03-31 22:26:53 -0500

    Update Pausable.sol

--- deps/Pausable.sol
+++ deps/Pausable.sol
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
 
-pragma solidity ^0.6.9;
-
-import {Ownable} from "https://raw.githubusercontent.com/sharedStake-dev/badger-system/master/deps/%40openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/access/Ownable.sol";
 
 /**
  * @title Pausable
